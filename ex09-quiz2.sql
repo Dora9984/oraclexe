14fa35fe19855c152cf08a63154576c010fdc8fe
Dora9984 <silbee08@gmail.com>
2023-08-15 13:33:10 +0900

    quiz and

--- ex09-quiz2.sql
+++ ex09-quiz2.sql
@@ -68,15 +68,49 @@ JOIN employees om ON oe.manager_id = om.employee_id
 
 -------- 여기부터
 -- 10. 직원들 중에서 급여(salary)가 10000 이상인 직원들의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
-SELECT e.last_name, d.department_name, e.salary
+SELECT e.last_name, d.department_name
 FROM employees e 
 JOIN departments d ON e.department_id = d.department_id
 WHERE e.salary >= 10000
 ORDER BY e.salary DESC
 ;
+
+SELECT * FROM departments;
+SELECT * FROM employees;
 /*
 11.
 각 부서의 이름(department_name), 해당 부서의 매니저의 ID(manager_id)와 매니저의 성(last_name),
 직원의 ID(employee_id), 직원의 성(last_name), 그리고 해당 직원의 급여(salary) 조회하기.
 직원들의 급여(salary)가 해당 부서의 평균 급여보다 높은 직원들을 조회합니다.
-결과는 부서 이름과 직원의 급여가 높은 순으로 정렬됩니다.
+결과는 부서 이름과 직원의 급여가 높은 순으로 정렬됩니다. -- 아주 어렵다... 혼자 천천히 다시 해보기
+*/
+SELECT d.department_name, d.manager_id, m.last_name AS MGR_LAST_NAME,
+        e.employee_id, e.last_name AS EMP_LAST_NAME, e.salary
+FROM departments d 
+JOIN employees m ON d.manager_id = m.employee_id
+JOIN employees e ON d.department_id = e.department_id
+WHERE 1 = 1
+AND e.salary > (
+                -- 각 부서의 평균 급여
+                SELECT AVG(e1.salary)
+                FROM employees e1
+                WHERE e1.department_id = d.department_id
+                )
+ORDER BY d.department_name, e.salary DESC;
+
+-- 평균 급여 GROUP BY로 구하기
+SELECT d.department_name, d.manager_id, m.last_name AS MGR_LAST_NAME,
+        e.employee_id, e.last_name AS EMP_LAST_NAME, e.salary
+FROM departments d 
+JOIN employees m ON d.manager_id = m.employee_id
+JOIN employees e ON d.department_id = e.department_id
+JOIN 
+    (
+    SELECT department_id, AVG(salary) AS AVG_SALARY
+    FROM employees
+    GROUP BY department_id
+    ) da
+ON d.department_id = da.department_id
+WHERE e.salary > da.AVG_SALARY
+ORDER BY d.department_name, e.salary DESC;
+
---
+++ ex13-DML.sql
@@ -0,0 +1,64 @@
+/*
+파일명 : ex13-DML.sql
+
+DML(Data Manipulation Language)
+    DB에서 데이터를 조작하고 처리하는 SQL
+    
+    INSERT문 : 테이블에 새로운 레코드 삽입
+    UPDATE문 : 테이블에 기존 레코드를 갱신(업데이트) 하는데 사용
+    DELETE문 : 테이블에서 특정 레코드를 삭제
+    
+    SELECT는 DML 포함될 수 있지만, 보통 DQL(Date Query Language)로 분류한다.
+*/
+
+/*
+INSERT 문
+[기본형식]
+    INSERT INTO 테이블명 (컬럼명1, 컬럼명2, ...)
+    VALUES(값1, 값2, ...);
+    
+    또는
+    
+    INSERT INTO 테이블명 (컬럼명1, 컬럼명2, ...) subquery;
+*/
+SELECT * FROM departments;
+
+INSERT INTO departments(department_id, department_name, manager_id, location_id)
+VALUES (280, 'Public Relations', 100, 1700);
+
+COMMIT; -- DML 결과를 영구적으로 DB에 반영. COMMIT 까지 해야 다른사람들이 볼 때도 반영됨.
+
+-- NULL 값을 가진 행 삽입
+-- 열 생략
+INSERT INTO departments (department_id, department_name)
+VALUES (290, 'Purchasing');
+
+ROLLBACK; -- DML문의 실행결과를 취소할 때(COMMIT 안했다는 가정 하에)
+
+/* INSERT subquery
+
+*/
+CREATE TABLE sales_reps
+AS (SELECT employee_id id, last_name name, salary, commission_pct
+    FROM employees
+    WHERE 1 = 2
+    )
+;
+
+SELECT * FROM sales_reps;
+
+-- job_id REP 포함된 사원
+SELECT employee_id, last_name, salary, commission_pct
+FROM employees
+WHERE job_id LIKE '%REP%'
+;
+
+INSERT INTO sales_reps(id, name, salary, commission_pct)
+SELECT employee_id, last_name, salary, commission_pct
+FROM employees
+WHERE job_id LIKE '%REP%'
+;
+
+COMMIT;
+
+SELECT * FROM sales_reps;
